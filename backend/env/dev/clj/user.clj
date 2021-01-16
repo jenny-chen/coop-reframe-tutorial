cf94171c223afa0f175e8177082f47c964407508
luke <nerorevenge@gmail.com>
2021-01-15 22:13:54 -0500

    Boiler plate backend code

---
+++ backend/env/dev/clj/user.clj
@@ -0,0 +1,63 @@
+(ns user
+  "Userspace functions you can run by default in your local REPL."
+  (:require
+   [backend.config :refer [env]]
+    [clojure.pprint]
+    [clojure.spec.alpha :as s]
+    [expound.alpha :as expound]
+    [mount.core :as mount]
+    [backend.core :refer [start-app]]
+    [backend.db.core]
+    [conman.core :as conman]
+    [luminus-migrations.core :as migrations]))
+
+(alter-var-root #'s/*explain-out* (constantly expound/printer))
+
+(add-tap (bound-fn* clojure.pprint/pprint))
+
+(defn start
+  "Starts application.
+  You'll usually want to run this on startup."
+  []
+  (mount/start-without #'backend.core/repl-server))
+
+(defn stop
+  "Stops application."
+  []
+  (mount/stop-except #'backend.core/repl-server))
+
+(defn restart
+  "Restarts application."
+  []
+  (stop)
+  (start))
+
+(defn restart-db
+  "Restarts database."
+  []
+  (mount/stop #'backend.db.core/*db*)
+  (mount/start #'backend.db.core/*db*)
+  (binding [*ns* (the-ns 'backend.db.core)]
+    (conman/bind-connection backend.db.core/*db* "sql/queries.sql")))
+
+(defn reset-db
+  "Resets database."
+  []
+  (migrations/migrate ["reset"] (select-keys env [:database-url])))
+
+(defn migrate
+  "Migrates database up for all outstanding migrations."
+  []
+  (migrations/migrate ["migrate"] (select-keys env [:database-url])))
+
+(defn rollback
+  "Rollback latest database migration."
+  []
+  (migrations/migrate ["rollback"] (select-keys env [:database-url])))
+
+(defn create-migration
+  "Create a new up and down migration file with a generated timestamp and `name`."
+  [name]
+  (migrations/create name (select-keys env [:database-url])))
+
+
